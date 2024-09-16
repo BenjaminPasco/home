72eec67a957881d61dc5f1852999d300c5f3ba55
bpasco <benjamin.pasco@hotmail.fr>
2024-09-16 23:46:27 +0200

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { vitePlugin } from "@remix-run/dev";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+	plugins: [vitePlugin(), tsconfigPaths()],
+});
